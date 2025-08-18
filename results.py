14692f653dec97b06b7c79fa976e9f3cd077006e
abonillao98 <138689547+abonillao98@users.noreply.github.com>
2025-08-18 18:12:22 +0200

    results initiation

--- results.py
+++ results.py
@@ -7,3 +7,32 @@ Given a match file, this script shows the overall performances of the CBIR accor
 the precision/recall measures and the F-score
 '''
 
+matches_file = "matches2.txt"
+
+f = open(matches_file)
+all_matches = f.read()
+f.close()
+
+
+lines = all_matches.count('\n')
+querys = int(lines/12)
+
+f = open(matches_file)
+for x in range(0,querys):
+    print("query number " + str(x))
+    matches=[]
+    for x in range(0,12):
+        a = re.findall(r'\d+', f.readline())
+        if a:
+            matches.append(int(a[0]))
+            
+    print(matches)
+    
+
+        
+
+    
+
+
+
+
